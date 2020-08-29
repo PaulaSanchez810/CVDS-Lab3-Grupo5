0e923677ed59f5ada6b1884c33dbc311fe4e41dc
PaulaSanchez <46799459+PaulaSanchez810@users.noreply.github.com>
2020-08-29 03:51:19 -0500

    correcion pom

--- ClasesEquivalencia/src/test/java/edu/edi/cvds/tdd/aerodescuentos/TarifasTes.java
+++ ClasesEquivalencia/src/test/java/edu/edi/cvds/tdd/aerodescuentos/TarifasTes.java
@@ -1,4 +1,13 @@
 package edu.edi.cvds.tdd.aerodescuentos;
+import org.junit.Assert;
+import org.junit.Test;
+
 
 public class TarifasTes {
+    @Test
+    public void CalculoTarifas(){
+        double delta = 0.01d;
+
+    }
+
 }
